3561b727b8c3a8e03e5ecdd1b9ff44f4b2dc66cc
TUIL Alexandre <tuil_alexandre@hotmail.com>
2017-02-06 23:56:47 +0100

    Nav retirée: conflit bxslider js/Logos comp ok/ajout de typed js

--- js/custom.js
+++ js/custom.js
@@ -2,43 +2,54 @@ $(document).ready(function(){
 
   //**** HIDE ON LOAD ****//
   // window.matchMedia('@media screen and (max-width: 700px)'); non fonctionnel why?
-
+// <!-- NAV & MENU BURGER comment: conflit with bxslider js -->
   // $('nav').hide();
-  $('.fa-times').hide();
+  // $('.fa-times').hide();
   $('#photo').hide();
   $('#contact-form2').hide();
 
   //**** MENU BURGER ****//
-  $('.fa-bars').click(function(){
-    $('nav').slideToggle('slow', function(){
-      $('.fa-bars').hide();
-      $('.fa-times').show();
-    });
-  });
+  // $('.fa-bars').click(function(){
+  //   $('nav').slideToggle('slow', function(){
+  //     $('.fa-bars').hide();
+  //     $('.fa-times').show();
+  //   });
+  // });
 
-  $('.fa-times').click(function(){
-    $('nav').slideToggle('slow', function(){
-      $('.fa-times').hide();
-      $('.fa-bars').show();
-    });
-  });
+  // $('.fa-times').click(function(){
+  //   $('nav').slideToggle('slow', function(){
+  //     $('.fa-times').hide();
+  //     $('.fa-bars').show();
+  //   });
+  // });
 
   //Activation des btn nav
   // au clic sur un lien
-  $('a[href^="#"]').on('click', function(){
-     // bloquer le comportement par défaut: on ne rechargera pas la page
-    //  evt.preventDefault();
-     // enregistre la valeur de l'attribut  href dans la variable target
-  var target = $(this).attr('href');
-     /* le sélecteur $(html, body) permet de corriger un bug sur chrome
-     et safari (webkit) */
-  $('html, body')
-     // on arrête toutes les animations en cours
-     .stop();
-     /* on fait maintenant l'animation vers le haut (scrollTop) vers
-      notre ancre target */
-    //  .animate({scrollTop: $(target).offset().top}, 1000 );
-   });
+  // $('a[href^="#"]').on('click', function(){
+  //    // bloquer le comportement par défaut: on ne rechargera pas la page
+  //   //  evt.preventDefault();
+  //    // enregistre la valeur de l'attribut  href dans la variable target
+  // var target = $(this).attr('href');
+  //    /* le sélecteur $(html, body) permet de corriger un bug sur chrome
+  //    et safari (webkit) */
+  // $('html, body')
+  //    // on arrête toutes les animations en cours
+  //    .stop();
+  //    /* on fait maintenant l'animation vers le haut (scrollTop) vers
+  //     notre ancre target */
+  //   //  .animate({scrollTop: $(target).offset().top}, 1000 );
+  //  });
+
+//    $('a[href*="#"]:not([href="#"])').click(function() {
+//   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//     var target = $(this.hash);
+//     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//     if (target.length) {
+//       $('html, body').scrollLeft().stop();
+//       // return false;
+//     }
+//   }
+// });
 
   //**** BXSLIDER ****//
     $('.bxslider').bxSlider({
