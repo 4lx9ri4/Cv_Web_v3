f2c3699c0c8ecc640ee6a881b6648b1bf9457a34
4lx9ri4 <tuil_alexandre@hotmail.com>
2017-02-09 09:15:20 +0100

    connexion bdd

--- php/connexionbdd.php
+++ php/connexionbdd.php
@@ -2,7 +2,7 @@
 	// On définit les 4 variables nécessaires à la connexion MySQL :
 	$hostname = "tuilalexzutuil.mysql.db";
 	$user     = "tuilalexzutuil";
-	$password = "Monkeybd1904";
+	$password = "Monkeyzu1904";
 	$nom_base_donnees = "tuilalexzutuil";
 
 	// Connexion au serveur MySQL
